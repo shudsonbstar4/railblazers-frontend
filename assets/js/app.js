fd08842af315fe545063a36a7299bc3cd2e12864
Sarah Hudson <sarah@checkappointments.com>
2015-01-26 20:26:11 -0500

    Created edit event modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -94,7 +94,7 @@ app.controller("ChangeUserPlanController", ['$scope', function($scope){
 	
 }])
 
-app.controller("EventsController", function($scope, $http){
+app.controller("EventsController", function($scope, $http, $modal){
 	//getting dummy data
 
 	$scope.events = [
@@ -149,4 +149,35 @@ app.controller("EventsController", function($scope, $http){
 		"desc": "Bitters trust fund readymade, hella raw denim cronut listicle normcore PBR&B. Fashion axe gastropub American Apparel selfies, typewriter bespoke normcore Tumblr cornhole Echo Park cred church-key deep v scenester. Food truck aesthetic gluten-free flannel Pinterest, cardigan raw denim twee chambray scenester butcher. Stumptown twee organic mustache cronut. Portland health goth semiotics craft beer, sriracha food truck flannel synth lo-fi YOLO. Twee Portland pug cred, chia beard four dollar toast sriracha dreamcatcher vegan meh American Apparel fingerstache iPhone. Crucifix Carles pop-up, flannel bicycle rights hashtag food truck keytar irony Vice aesthetic gluten-free fingerstache hoodie."
 	}
 ]
+
 })
+
+app.controller('EditEventModalController', ['$scope', '$modal', function($scope, $modal){
+	$scope.openEditEvent = function(selectedEvent){
+	  var $modalInstance = $modal.open({ //Replace template with templateUrl when can deliver via HTTP
+	    template: "<div class='modal-header'><h4>Edit Event</h4></div><div class='modal-body'><label class='control-label'>Title:</label><input class='form-control' type='text' ng-model='selectedEvent.name' /><br /><label class='control-label'>Group:</label><p><span ng-bind='selectedEvent.group'></span></p></div><div class='modal-footer'><button class='btn btn-success green' ng-click='cancel()'>Save Event</button><button class='btn btn-default red' ng-click='cancel()'>Cancel Changes</button></div><div class='clear-space'></div>",
+	    controller: 'EditEventModalInstanceController',
+	    resolve: {
+			selectedEvent: function(){
+				return selectedEvent;
+			}
+		}//resolve
+
+	  })
+
+	  $modalInstance.result.then(function() {
+	      $log.info('Modal dismissed at: ' + new Date());
+	    });//modalInstance
+	};
+}])
+
+//EVENTS Modal
+app.controller('EditEventModalInstanceController', ['$scope', '$timeout', '$modal', '$modalInstance', '$http', 'selectedEvent', function($scope, $timeout, $modal, $modalInstance, $http, selectedEvent){
+
+	$scope.selectedEvent = selectedEvent;
+
+	$scope.cancel = function(){
+		$modalInstance.dismiss('cancel');
+	};
+
+}]);
